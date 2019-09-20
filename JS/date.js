35ce8c4568f0901cb467dc86ef2b9955e8770e85
Harper-Christopher <christopherharper123@gmail.com>
2019-09-19 20:22:25 -0600

    Update

--- JS/date.js
+++ JS/date.js
@@ -1,9 +1,15 @@
 function GetCurrentYear()
 {
     var date = new Date();
-    document.getElementById("currentyear").innerHTML = date.getFullYear();
+    document.getElementById("currentYear").innerHTML = date.getFullYear();
+}
+
+function ModifiedDate(){
+    var modified = new ModifiedDate();
+    document.getElementById("timeModified".innerHTML = alert(document.lastModified);
 }
 
 function RefreshDates(){
 GetCurrentYear();
+ModifiedDate();
 }
