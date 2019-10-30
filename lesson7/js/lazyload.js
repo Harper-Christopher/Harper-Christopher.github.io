aed37277554a59451888bbdcb6ecc1d3245c5133
Harper-Christopher <christopherharper123@gmail.com>
2019-10-29 23:05:33 -0600

    Lazy Load Update

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -1,3 +1,36 @@
+// Example used from https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Loading
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
+};
+
+if('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if(item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
+    });
+
+    imagesToLoad.forEach((img) => {
+      observer.observe(img);
+    });
+  } 
+  
+  else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
+  }
+
+
+  // Alternate example from https://www.youtube.com/watch?v=mC93zsEsSrg
+  /*
 const images = document.querySelectorAll("[data-src]");
 
 function preloadImage(img) {
@@ -29,3 +62,4 @@ const imgObserver = new IntersectionObserver((entries, imgObserver) => {
 images.forEach(image => {
     imgObserver.observe(image);
 });
+*/
