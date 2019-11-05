1b54cc9a68d70bf154623413b9bc5013c3f4b481
Harper-Christopher <christopherharper123@gmail.com>
2019-11-04 17:16:06 -0700

    Starting weather center page

---
+++ lesson8/js/windchill.js
@@ -0,0 +1,11 @@
+var high = parseInt(document.getElementById("high").innerHTML);
+var windSpeed = parseInt(document.getElementById("windspeed").innerHTML);
+var windMilesPerHour = windSpeed * 0.16;
+var windChillFactor = 35.74 + 0.6215 * high - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * high * Math.pow(windSpeed, 0.16);
+
+if (high <= 50 && windSpeed >= 3) {
+document.getElementById("windchill").innerHTML = Math.round(windChillFactor) + "° F";
+}
+else {
+document.getElementById("windchill").innerHTML = "N/A";
+}
