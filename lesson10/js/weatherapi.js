b029b90c08608ace3ea17a93c7b76aff985478f4
Harper-Christopher <christopherharper123@gmail.com>
2019-11-19 16:59:43 -0700

    update

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,5 +1,5 @@
 // Using openweathermap.org to get weather data. (Pulling from weather?, id=5604473 is the code for Preston, with APPID being the key given for signing up, and units=imperial turns temp to F.)
-const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=fe9fd1b6278811d8a022a086d5b14b88&units=imperial";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=fe9fd1b6278811d8a022a086d5b14b88&units=imperial";
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
@@ -16,7 +16,7 @@ fetch(apiURL)
 
 
 document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
-document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+document.getElementById("icon").setAttribute('src', imagesrc); // focus on the setAttribute() method
 document.getElementById('icon').setAttribute('alt', desc);
 
 
