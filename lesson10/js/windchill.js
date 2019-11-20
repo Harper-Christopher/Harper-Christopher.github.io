2942643634b4cdfafae027d13575b6940f57c8e7
Harper-Christopher <christopherharper123@gmail.com>
2019-11-19 22:27:34 -0700

    Update Home

--- lesson10/js/windchill.js
+++ lesson10/js/windchill.js
@@ -1,15 +1,18 @@
-/* JavaScript to calculate the windchill in the weather summary. */
-var high = parseInt(document.getElementById("high").innerHTML);
-var windSpeed = parseInt(document.getElementById("windspeed").innerHTML);
-var windMilesPerHour = windSpeed * 0.16;
-var windChillFactor = 35.74 + 0.6215 * high - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * high * Math.pow(windSpeed, 0.16);
+    /* JavaScript to calculate the windchill in the weather summary. */
+    /* (Old static formula for weather summary)
+    var high = parseInt(document.getElementById("high").innerHTML);
+    var windSpeed = parseInt(document.getElementById("windspeed").innerHTML);
+    var windMilesPerHour = windSpeed * 0.16;
+    var windChillFactor = 35.74 + 0.6215 * high - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * high * Math.pow(windSpeed, 0.16);
+    
+    if (high <= 50 && windSpeed >= 3) {
+    document.getElementById("windchill").innerHTML = Math.round(windChillFactor) + "° F";
+    }
+    else {
+    document.getElementById("windchill").innerHTML = "N/A";
+    }*/
 
-if (high <= 50 && windSpeed >= 3) {
-document.getElementById("windchill").innerHTML = Math.round(windChillFactor) + "° F";
-}
-else {
-document.getElementById("windchill").innerHTML = "N/A";
-}
+
 
 /* JavaScript to display current conditions in the weather summary. */
 const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=fe9fd1b6278811d8a022a086d5b14b88&units=imperial";
@@ -19,26 +22,43 @@ fetch(apiURL)
     console.log(jsObject);
 
     document.getElementById('currently').textContent = jsObject.weather[0].description;
-    document.getElementById('high').textContent = jsObject.main.temp + "° F" + " / " + jsObject.main.temp_max + "° F" ;
+    document.getElementById('high').textContent = Math.round(jsObject.main.temp) + "° F";
     document.getElementById('humidity').textContent = jsObject.main.humidity + "%";
-    document.getElementById('windspeed').textContent = jsObject.wind.speed + " mph";
+    document.getElementById('windspeed').textContent = Math.round(jsObject.wind.speed) + " mph";
 
+/* JavaScript to calculate the windchill in the weather summary. */
+var high = jsObject.main.temp;
+var windSpeed = jsObject.wind.speed;
+var windMilesPerHour = windSpeed * 0.16;
+var windChillFactor = 35.74 + 0.6215 * high - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * high * Math.pow(windSpeed, 0.16);
+
+if (high <= 50 && windSpeed >= 3) {
+document.getElementById("windchill").innerHTML = Math.round(windChillFactor) + "° F";
+}
+else {
+document.getElementById("windchill").innerHTML = "N/A";
+}
 });
 
-const apiURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=fe9fd1b6278811d8a022a086d5b14b88&units=imperial";
-fetch(apiURL)
+
+
+
+// JavaScript to display data for the 5 day forecast.
+const forecastURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=fe9fd1b6278811d8a022a086d5b14b88&units=imperial";
+fetch(forecastURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
 
 
-    for (let i = 0; i < forecast.list.length; i++ ) {
+    //for (let i = 0; i < forecast.list.length; i++ ) {
 
-    if (jsObject.forecast.list.dt_txt.includes("18:00:00") {
+    //if (jsObject.forecast.list[0].dt_txt.includes("18:00:00") {
 
-    }
+    //}
 
-}
+//}
+
   });
 
   var currentDate = new Date();
