62ce2118981da35ccd50cf1ad92d98b07144023b
Harper-Christopher <christopherharper123@gmail.com>
2019-11-21 22:17:33 -0700

    Lesson 11

---
+++ lesson11/js/weatherapi.js
@@ -0,0 +1,25 @@
+// Using openweathermap.org to get weather data. (Pulling from weather?, id=5604473 is the code for Preston, with APPID being the key given for signing up, and units=imperial turns temp to F.)
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=fe9fd1b6278811d8a022a086d5b14b88&units=imperial";
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+
+
+    // Displays the temp by using the JS object created pulls the data from the main, and uses the temp value.
+    document.getElementById('current-temp').textContent = jsObject.main.temp;
+
+    // Creating variable for an image directly from the openweathermap website, uses the JS object in weather getting the icon in the first position [0] and making it a .png.
+  const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';
+
+  // Creating variable for the discription in the first weather position [0].
+  const desc = jsObject.weather[0].description; 
+
+
+document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+document.getElementById("icon").setAttribute('src', imagesrc); // focus on the setAttribute() method
+document.getElementById('icon').setAttribute('alt', desc);
+
+
+  });
+
---
+++ lesson11/js/windchill.js
@@ -0,0 +1,60 @@
+/* JavaScript to display current conditions in the weather summary. */
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=fe9fd1b6278811d8a022a086d5b14b88&units=imperial";
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((weatherObject) => {
+    console.log(weatherObject);
+
+
+    document.getElementById('currently').textContent = weatherObject.weather[0].description.toUpperCase();
+    document.getElementById('high').textContent = Math.round(weatherObject.main.temp_max) + "° F";
+    document.getElementById('currentTemp').textContent = Math.round(weatherObject.main.temp) + "° F";
+    document.getElementById('humidity').textContent = weatherObject.main.humidity + "%";
+    document.getElementById('windspeed').textContent = Math.round(weatherObject.wind.speed) + " mph";
+
+/* JavaScript to calculate the windchill in the weather summary. */
+var high = weatherObject.main.temp;
+var windSpeed = weatherObject.wind.speed;
+var windMilesPerHour = windSpeed * 0.16;
+var windChillFactor = 35.74 + 0.6215 * high - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * high * Math.pow(windSpeed, 0.16);
+
+if (high <= 50 && windSpeed >= 3) {
+document.getElementById("windchill").innerHTML = Math.round(windChillFactor) + "° F";
+}
+else {
+document.getElementById("windchill").innerHTML = "N/A";
+}
+});
+
+
+
+
+// JavaScript to display data for the 5 day forecast.
+const forecastURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=fe9fd1b6278811d8a022a086d5b14b88&units=imperial";
+fetch(forecastURL)
+  .then((response) => response.json())
+  .then((forecastObject) => {
+    console.log(forecastObject);
+
+// plusOne variable to increase the element ID each time through the for loop to place data in the next element.    
+plusOne = 1;
+
+    for (let i = 0; i < forecastObject.list.length; i++) {
+
+    if (forecastObject.list[i].dt_txt.includes("18:00:00")) {
+
+        let timeDate = forecastObject.list[i].dt;
+        let date = new Date(timeDate * 1000);
+        let weekDays = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
+        let dayOfWeek = weekDays[date.getDay()];
+
+        document.getElementById('day' + plusOne).textContent = dayOfWeek; 
+        document.getElementById('high' + plusOne).textContent = Math.round(forecastObject.list[i].main.temp_max) + "° F";
+        document.getElementById("pic" + plusOne).setAttribute('src', 'https://openweathermap.org/img/w/' + forecastObject.list[i].weather[0].icon + '.png'); 
+        document.getElementById('pic' + plusOne).setAttribute('alt', forecastObject.list[i].weather[0].description);
+        
+        plusOne++;
+    }
+}
+
+  });
